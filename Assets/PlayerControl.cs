17e389dbe56e11f76508b7a7be4418b176f5d834
Levi Rosenberg <levirosenberg97@gmail.com>
2018-08-22 17:57:30 -0700

    added a player controller

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+    Animator anim;
+    bool isMoving;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Move()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        
+
+        Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement);
+
+        Vector3 desiredVelocity = movement.normalized * speed;
+        //rb.AddForce(movement * speed);
+        rb.AddForce(desiredVelocity - rb.velocity, ForceMode.Impulse);
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("Speed", rb.velocity.magnitude);        
+        //Vector3.Lerp(rb.velocity, Vector3.zero, Time.deltaTime);
+        //if(Input.GetKey(KeyCode.W)||
+        //   Input.GetKey(KeyCode.A)||
+        //   Input.GetKey(KeyCode.S)||
+        //   Input.GetKey(KeyCode.D))
+        //{
+            //isMoving = true;
+
+            
+        //}
+        //else
+        //{
+           // isMoving = false;
+        //}
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+
+        //if (isMoving == true)
+        //{
+        //    Move();
+        //}
+        //else
+        //{
+        //    rb.velocity = Vector3.zero;
+        //}
+    }
+}
---
+++ Assets/Scripts/AnimationSpeedController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationSpeedController : MonoBehaviour
+{
+    public Slider speedSlider;
+    public Animator anim;
+
+	void Start ()
+    {
+        anim.speed = 0;
+	}	
+
+    public void BlendSpeed()
+    {
+        anim.speed = speedSlider.value;
+    }
+}
